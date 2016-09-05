f5fdce17c495ccad4989c8cb66c1c772af11427c
johno <johno>
2016-09-05 16:28:58 -0400

    cleanup logs in javascript files

--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,6 @@
-console.log("content.js");
-
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-	  
-	window.scrollBy(0,100);  
-	console.log("content.js v3");  
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
+  function(request, sender, sendResponse) {	  
+	var scroolDownAmount = request.scrollPixelAmt;
+	window.scrollBy(0,Number(scroolDownAmount));
+	sendResponse({success: true});
   });
--- scripts/extension.js
+++ scripts/extension.js
@@ -1,5 +1,3 @@
-console.log("extension.js");
-
 function getSpeed(){
 	var speed = 3000;
 	var options = document.getElementsByName("speed");
@@ -15,14 +13,13 @@ function getSpeed(){
 
 var app = new (function(){
 	var self = this;
+	self.scrollPixelAmt = 100;
 	self.isScrolling = false,
 	self.text = {
 		setBtn : function(){
 			var SCROLL = "scroll";
 			var STOP = "stop";			
 			var updatedText = (self.isScrolling)? STOP : SCROLL;
-
-			console.log("this.isScrolling " + self.isScrollicng);
 			document.getElementById('toggleScroll').innerText=updatedText;
 		}
 	}
@@ -33,8 +30,8 @@ var app = new (function(){
 	self.run=function(){		
 		console.log("extension.js v3");
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-			console.log(response.farewell);
+		  chrome.tabs.sendMessage(tabs[0].id, {scrollPixelAmt: self.scrollPixelAmt}, function(response) {
+			console.log(response);
 		  });
 		});
 
